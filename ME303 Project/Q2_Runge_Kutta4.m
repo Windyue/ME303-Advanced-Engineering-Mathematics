a5455f98fbdc68cbbbe220d610c1c1cc042c916f
ahtchow <ahtchow@gmail.com>
2021-03-07 18:48:52 -0500

    rk4 q2

---
+++ ME303 Project/Q2_Runge_Kutta4.m
@@ -0,0 +1,34 @@
+clc
+clear
+close all
+
+% Constants
+rho = 1000;
+P_o = 9910000;
+sigma = 0.07495;
+mu = 0.0015215;
+
+% Initial Values
+R_cur = 100;
+V_cur = 0;
+
+i= 10000;
+dt = 1e-8;
+t = linspace(0,dt*i,i+1);
+
+
+R = [] * i+1;
+
+for c = 1 :i+1
+    [R_cur, V_cur] = RK4_rayleigh_plesset(R_cur, V_cur, rho, sigma, mu, P_o, dt);
+    fprintf('\n c=%d, R_cur=%d, V_cur=%d' , c, R_cur ,V_cur); 
+    R(c) = R_cur;
+    
+end
+
+plot(t,R);
+grid on;
+xlabel('t');
+ylabel('R');
+
+
--- ME303 Project/RK4_rayleigh_plesset.m
+++ ME303 Project/RK4_rayleigh_plesset.m
@@ -1,29 +1,31 @@
-function [R_fwd, V_fwd] = RK4(R_cur, V_cur, rho, dt, lambda_sqrd, R_o, P_o)
+function [R_fwd, V_fwd] = RK4_rayleigh_plesset(R_cur, V_cur, rho, sigma, mu, P_o, dt)
 
 % RK2: y_i+1 = y_i + Δt * (( f(t,y)* + f(t,y)) / 2 )
 
 % (1): R_i+1 = Ri + Δt * R_i'
 % (2): V_i+1 = Vi + Δt * V_i'
 
+V_slope=@(R, V)((-P_o - ((4*mu*V)/R) - ((2*sigma)/R) - (1.5*V*V*rho) ) / (R*rho));
+
 % R at t_i+1/2 using i values
 R1 = R_cur + (dt) * V_cur;
-V1_slope =  (lambda_sqrd * (R_o - R_cur) - 1.5 * (P_o / (rho*R_o)));
+V1_slope =  V_slope(R_cur, V_cur);
 V1 = V_cur + (dt) * V1_slope;
 
 % R at t_i+1/2 using i+1/2* values
-R2_slope = V1;
+R2_slope = V1; 
 R2 = R_cur + (dt/2) * R2_slope; 
-V2_slope =  (lambda_sqrd * (R_o - R1) - 1.5 * (P_o / (rho*R_o)));
+V2_slope =  V_slope(R1, V1);
 V2 = V_cur + (dt/2) * V2_slope;
 
 % R at t_i+1/2 using i+1/2** values
 R3_slope = V2;
 R3 = R_cur + (dt/2) * R3_slope;
-V3_slope = (lambda_sqrd * (R_o - R2) - 1.5 * (P_o / (rho*R_o)));
+V3_slope = V_slope(R2, V2);
 V3 = V_cur + (dt/2) * V3_slope;
 
 % R at t_i+1 using i+1/2** values
-V4_slope = (lambda_sqrd * (R_o - R3) - 1.5 * (P_o / (rho*R_o)));
+V4_slope = V_slope(R3, V3);
 V4 =  V_cur + (dt) * V4_slope;
 
 % Modified Slopes
